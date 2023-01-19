845ccb653838996ed5b32274c19e4826c12d8075
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-01-18 20:39:53 -0500

    building searchbar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
+
+
+const search = document.getElementById('search');
+search.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowercase();
+    console.log(currentValue);
+});
